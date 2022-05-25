ae7ebfd7f9ba2a85e4509285fcbf6305ce6cf2cb
Ruby1209 <ruby.s.bharadwaj@gmail.com>
2022-05-25 12:23:46 +0530

    Added Pipeline Code

--- aws_python_cdk_pipeline/aws_python_cdk_pipeline_stack.py
+++ aws_python_cdk_pipeline/aws_python_cdk_pipeline_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    aws_codepipeline,
+    aws_codepipeline_actions,
+    aws_ssm,
+    aws_codebuild,
+    App, CfnOutput,SecretValue, Stack
 )
 from constructs import Construct
 
@@ -9,11 +11,36 @@ class AwsPythonCdkPipelineStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsPythonCdkPipelineQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        pipeline = aws_codepipeline.Pipeline(self, "Pipeline",pipeline_name="AWSPythonPipeline")
+        cdk_source_output = aws_codepipeline.Artifact()
+        cdk_source_action = aws_codepipeline_actions.GitHubSourceAction(
+                            action_name="CDK_GitHub_Source",
+                            owner="Ruby1209",
+                            repo="aws-python-cdk-pipeline",
+                            oauth_token=SecretValue.secrets_manager("github_access_token"),
+                            output=cdk_source_output,
+                            branch="master"
+            )
+        pipeline.add_stage(
+                        stage_name="Source",
+                        actions=[cdk_source_action]
+            )
+        project = aws_codebuild.PipelineProject(self, "AWSPythonPipelineProject",
+                            build_spec = aws_codebuild.BuildSpec.from_source_filename(
+                                 "/cdk_buildspec.yml"
+                        )
+            )
+        cdk_build_action = aws_codepipeline_actions.CodeBuildAction(
+                            action_name="CDK_CodeBuild",
+                            project=project,
+                            input=cdk_source_output,
+                            environment_variables={
+                                "COMMIT_URL": aws_codebuild.BuildEnvironmentVariable(
+                                    value=cdk_source_action.variables.commit_url
+                                )
+                            }
+            )
+        pipeline.add_stage(
+                        stage_name="Build",
+                        actions=[cdk_build_action] 
+            )      
